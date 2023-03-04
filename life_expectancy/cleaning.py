b92800c968b9da9d19539a550345ba400a1b4115
Nuno Filipe Paiva <nuno.paiva@nos.pt>
2023-03-04 11:51:01 +0000

    pylint corrections

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,31 +1,47 @@
+"""
+This module provides a function to clean european life expectancy data files.
+"""
+
 import re
 import pandas as pd
 
 # Define input and output file paths
-input_file_path = './life_expectancy/data/eu_life_expectancy_raw.tsv'
-output_file_path = './life_expectancy/data/pt_life_expectancy.csv'
+INPUT_FILE_PATH = './life_expectancy/data/eu_life_expectancy_raw.tsv'
+OUTPUT_FILE_PATH = './life_expectancy/data/pt_life_expectancy.csv'
 
 # Define variables cleaning  and filtering data
 id_vars = ['unit', 'sex', 'age','region']
-region_filter='pt'
+REGION_FILTER='PT'
 
 def clean_data() -> None:
+    """
+    This function processes and filters a file with european life expectancy data over the years.
+
+    Parameters:
+    None
+
+    Returns:
+    Just saves the processed data for Portugal.
+    """
     # load data
-    df_raw = pd.read_csv(input_file_path)
+    df_raw = pd.read_csv(INPUT_FILE_PATH)
 
     # Split column that has region and years data into separate columns
     df_geoyears_cols=df_raw.iloc[:,3].str.split('\t', expand=True)
     df_geoyears_cols.columns=df_raw.columns[3].split('\t')
     df_final=pd.concat([df_raw.iloc[:,0:3],df_geoyears_cols], axis=1)
     df_final.columns.values[3]='region'
-    
-    #Transform data into long format, filter out missings and region
+
+    # Transform data into long format, filter out missings and region
     df_final = pd.melt(df_final,id_vars=id_vars)
-    df_final = pd.concat([df_final[df_final['value']!=': '].iloc[:,:5],df_final[df_final['value']!=': '].iloc[:,5].apply(lambda x: re.search(r'\d+(?:\.\d+)?',x).group())], axis=1)
-    df_final = df_final[(df_final['region']==region_filter)]
+    df_final = pd.concat([df_final[df_final['value']!=': '].iloc[:,:5],
+        df_final[df_final['value']!=': '].iloc[:,5].
+            apply(lambda x: re.search(r'\d+(?:\.\d+)?',x).group())], axis=1)
+    df_final.columns.values[4]='year'
+    df_final = df_final[(df_final['region']==REGION_FILTER)]
 
     # Save cleaned data to output file
-    df_final.to_csv(output_file_path, index=False)
-py
+    df_final.to_csv(OUTPUT_FILE_PATH, index=False)
+
 if __name__ == '__main__':
     clean_data()
