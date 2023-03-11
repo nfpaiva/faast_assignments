18694d5c2eee8ad8bf27cdb199b3e8d7791d2452
Nuno Filipe Paiva <nuno.paiva@nos.pt>
2023-03-11 13:00:58 +0000

    added vectorization fix and error handling

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -3,7 +3,6 @@ This module provides a function to clean european life expectancy data files.
 """
 
 from pathlib import Path
-import re
 import argparse
 import pandas as pd
 
@@ -29,8 +28,12 @@ def clean_data(region_filter: str) -> None:
     Returns:
     Just saves the processed data for Portugal.
     """
-    # load data
-    df_raw = pd.read_csv(INPUT_FILE_PATH)
+    try:
+        # load data
+        df_raw = pd.read_csv(INPUT_FILE_PATH)
+    except FileNotFoundError:
+        print(f"ERROR: Input file {INPUT_FILE_PATH} not found.")
+        return
 
     # get name of 3rd column that has region and years data
     col_geo_years = df_raw.columns[3]
@@ -43,16 +46,24 @@ def clean_data(region_filter: str) -> None:
 
     # Transform data into long format, filter out missings and region
     df_final = pd.melt(df_final,id_vars=id_vars, var_name='year')
-    # df_final.columns.values[df_final.columns.get_loc('variable')]='year'
-    df_final = pd.concat([df_final[df_final['value']!=': '][id_vars + ['year']],
-                          df_final[df_final['value']!=': ']['value'].
-                          apply(lambda x: re.search(r'\d+(?:\.\d+)?',x).group())]
-                          ,axis=1)
+    df_final['value'] = df_final['value'].str.extract(r'(\d+(?:\.\d+)?)', expand=False)
+    df_final = df_final[df_final['value'].notna()]
 
     df_final = df_final[(df_final['region']==region_filter)]
 
-    # Save cleaned data to output file
-    df_final.to_csv(OUTPUT_FILE_PATH, index=False)
+    if df_final.empty:
+        print(f"No data found for region {region_filter}")
+        return
+
+    try:
+        # Save cleaned data to output file
+        df_final.to_csv(OUTPUT_FILE_PATH, index=False)
+    except PermissionError:
+        print(f"ERROR: Output file {OUTPUT_FILE_PATH} could not be created or written to.")
+        return
+
+    print(f"Successfully cleaned data for region {region_filter}.")
+
 
 if __name__ == '__main__':# pragma: no cover
     parser = argparse.ArgumentParser(description='Clean European life expectancy data')
