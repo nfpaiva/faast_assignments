30b340277d545bebd1003e32f7d4449e72752ab6
Nuno Filipe Paiva <nuno.paiva@nos.pt>
2023-03-17 13:05:00 +0000

    adding Fernando code review suggestions

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -2,9 +2,10 @@
 This module provides a function to clean european life expectancy data files.
 """
 
-from pathlib import Path
 import argparse
 import logging
+from pathlib import Path
+
 import pandas as pd
 
 # Set up logging
@@ -15,64 +16,57 @@ FILE_PATH = Path(__file__).resolve()
 BASE_PATH = FILE_PATH.parent
 
 # Define input and output file paths relative to base path
-INPUT_FILE_PATH = BASE_PATH / 'data' / 'eu_life_expectancy_raw.tsv'
-OUTPUT_FILE_PATH = BASE_PATH  / 'data' / 'pt_life_expectancy.csv'
+INPUT_FILE_PATH = BASE_PATH / "data" / "eu_life_expectancy_raw.tsv"
+OUTPUT_FILE_PATH = BASE_PATH / "data" / "pt_life_expectancy.csv"
 
 # Define variables cleaning  and filtering data
-COMPOSED_COL = 'unit,sex,age,geo\\time'
-DECOMPOSED_COLS= ['unit', 'sex', 'age','region']
+COMPOSED_COL = "unit,sex,age,geo\\time"
+DECOMPOSED_COLS = ["unit", "sex", "age", "region"]
 
-def load_data (input_file_path: Path = INPUT_FILE_PATH) -> pd.DataFrame:
+
+def load_data(input_file_path: Path = INPUT_FILE_PATH) -> pd.DataFrame:
     """
     This function loads a file with european life expectancy data over the years.
 
     Parameters:
-    input_file_path (str): The path to the input file.
+    input_file_path (Path): The path to the input file.
 
     Returns:
-    df_raw (pd.DataFrame): The loaded data.
+    pd.DataFrame: The loaded data.
     """
-    df_raw = pd.DataFrame()  # define an empty DataFrame
 
-    try:
-        # load data
-        with open(input_file_path, encoding='utf-8') as filename:
-            df_raw = pd.read_csv(filename, sep='\t', na_values=[':'])
-    except PermissionError:# pragma: no cover
-        logging.error("Permission denied to read %s.", input_file_path)
-        return df_raw
-    except FileNotFoundError as error:# pragma: no cover
-        logging.error(str(error))
-        return df_raw
-    except pd.errors.ParserError as error:# pragma: no cover
-        logging.error("Error reading file %s: %s", input_file_path, error)
-        return df_raw
-    return df_raw
+    return pd.read_csv(input_file_path, sep="\t", na_values=[":"], encoding="utf-8")
+
 
 def __convert_datatypes(dataframe: pd.DataFrame) -> pd.DataFrame:
     """
-        This function iterates on each group of columns to convert to the determined data type
+    This function iterates on each group of columns to convert to the determined data type
     """
-    #convert column data types explicitly all with exception of "value"
+    # convert column data types explicitly all with exception of "value"
     data_types = {
-        'unit': 'category',
-        'sex': 'category',
-        'age': 'category',
-        'region': 'category',
-        'year': 'int'
+        "unit": "category",
+        "sex": "category",
+        "age": "category",
+        "region": "category",
+        "year": "int",
     }
     try:
         dataframe = dataframe.astype(data_types)
-    except ValueError as error:# pragma: no cover
-        logging.error("Datatype conversion error %s", error)
-    #convert value column to numeric
+    except ValueError as err:  # pragma: no cover
+        logging.error("Datatype conversion error %s", err)
+        raise ValueError from err
+    # convert value column to numeric
     try:
-        dataframe['value'] = pd.to_numeric(dataframe['value']\
-                                                .str.extract(r'(\d+(?:\.\d+)?)',
-                                                            expand=False), errors='raise')
-    except ValueError as error:# pragma: no cover
-        logging.error("Datatype conversion error %s", error)
+        dataframe["value"] = pd.to_numeric(
+            dataframe["value"].str.extract(r"(\d+(?:\.\d+)?)", expand=False),
+            errors="raise",
+        )
+    except ValueError as err:  # pragma: no cover
+        logging.error("Datatype conversion error %s", err)
+        raise ValueError from err
+
     return dataframe
+
 
 def clean_data(df_raw: pd.DataFrame, region_filter: str) -> pd.DataFrame:
     """
@@ -87,29 +81,33 @@ def clean_data(df_raw: pd.DataFrame, region_filter: str) -> pd.DataFrame:
     """
 
     # Split first column into 4 columns
-    df_raw[DECOMPOSED_COLS]=df_raw[COMPOSED_COL].str.split(',', expand=True)
+    df_raw[DECOMPOSED_COLS] = df_raw[COMPOSED_COL].str.split(",", expand=True)
     df_raw = df_raw.drop(columns=[COMPOSED_COL])
 
     df_final = pd.DataFrame()  # define an empty DataFrame
 
     # Transform data into long format, filter out missings and region
-    df_final = pd.melt(df_raw,id_vars=DECOMPOSED_COLS, var_name='year')
+    df_final = pd.melt(df_raw, id_vars=DECOMPOSED_COLS, var_name="year")
 
     # Convert data types
-    df_final=__convert_datatypes(df_final)
-    df_final = df_final[df_final['region'] ==  region_filter]
-    df_final = df_final.dropna(subset=['value'])
+    df_final = __convert_datatypes(df_final)
+    df_final = df_final[df_final["region"] == region_filter]
+    df_final = df_final.dropna(subset=["value"])
 
-    df_final = df_final[(df_final['region']==region_filter)]
+    df_final = df_final[(df_final["region"] == region_filter)]
 
-    if df_final.empty:# pragma: no cover
+    if df_final.empty:  # pragma: no cover
         logging.error("No data found for region %s", region_filter)
         return df_final
 
     return df_final
 
-def save_data(df_final: pd.DataFrame, output_file_path: Path = OUTPUT_FILE_PATH ,
-              region_filter: str="") -> None:
+
+def save_data(
+    df_final: pd.DataFrame,
+    output_file_path: Path = OUTPUT_FILE_PATH,
+    region_filter: str = "",
+) -> None:
     """
     This function saves the cleaned data to a file.
 
@@ -124,11 +122,14 @@ def save_data(df_final: pd.DataFrame, output_file_path: Path = OUTPUT_FILE_PATH 
     try:
         # Save cleaned data to output file
         df_final.to_csv(output_file_path, index=False)
-    except PermissionError:# pragma: no cover
-        logging.error("Output file %s could not be created or written to.", output_file_path)
-        return
+    except PermissionError as err:  # pragma: no cover
+        logging.error(
+            "Output file %s could not be created or written to.", output_file_path
+        )
+        raise PermissionError from err
 
-    logging.info("Successfully saved data for region %s.",region_filter)
+    logging.info("Successfully saved data for region %s.", region_filter)
+
 
 def main(region_filter: str) -> None:
     """
@@ -136,18 +137,24 @@ def main(region_filter: str) -> None:
 
     Parameters:
     region (str): The region code to filter the data by.
-    
+
     Returns:
     None
 
     """
     save_data(
-        clean_data(df_raw=
-                   load_data(), region_filter=region_filter), region_filter=region_filter)
+        clean_data(df_raw=load_data(), region_filter=region_filter),
+        region_filter=region_filter,
+    )
 
-if __name__ == '__main__':# pragma: no cover
-    parser = argparse.ArgumentParser(description='Clean European life expectancy data')
-    parser.add_argument('--region', type=str, default='PT',
-                        help='The region code to filter the data by (default: PT)')
+
+if __name__ == "__main__":  # pragma: no cover
+    parser = argparse.ArgumentParser(description="Clean European life expectancy data")
+    parser.add_argument(
+        "--region",
+        type=str,
+        default="PT",
+        help="The region code to filter the data by (default: PT)",
+    )
     args = parser.parse_args()
-    main(region_filter = args.region)
+    main(region_filter=args.region)
